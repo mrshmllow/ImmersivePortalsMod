31473e9b601c581bac5721213b83efb20f2668d4
qouteall <qouteall@163.com>
2024-11-25 21:36:06 +0800

    WIP new sodium iris compat

--- src/main/java/qouteall/imm_ptl/core/compat/mixin/iris/MixinIrisSodiumSodiumTerrainPipeline.java
+++ src/main/java/qouteall/imm_ptl/core/compat/mixin/iris/MixinIrisSodiumSodiumTerrainPipeline.java
@@ -1,8 +1,6 @@
 package qouteall.imm_ptl.core.compat.mixin.iris;
 
 import com.mojang.blaze3d.shaders.Program;
-import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexType;
-import net.irisshaders.iris.pipeline.SodiumTerrainPipeline;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,51 +14,51 @@ import qouteall.q_misc_util.Helper;
 import java.util.Optional;
 
 @Pseudo
-@Mixin(value = SodiumTerrainPipeline.class, remap = false)
+//@Mixin(value = SodiumTerrainPipeline.class, remap = false)
 public class MixinIrisSodiumSodiumTerrainPipeline {
-    @Shadow
-    Optional<String> terrainSolidVertex;
-    
-    @Shadow
-    Optional<String> terrainCutoutVertex;
-    
-    @Shadow
-    Optional<String> translucentVertex;
-    
-    @Unique
-    private boolean immptlPatched = false;
-    
-    @Inject(
-        method = "patchShaders",
-        at = @At("RETURN")
-    )
-    private void onPatchShaderEnds(ChunkVertexType chunkVertexType, CallbackInfo ci) {
-        if (!immptlPatched) {
-            immptlPatched = true;
-            terrainSolidVertex = terrainSolidVertex.map(code ->
-                ShaderCodeTransformation.transform(
-                    Program.Type.VERTEX,
-                    "iris_sodium_terrain_vertex",
-                    code
-                )
-            );
-            terrainCutoutVertex = terrainCutoutVertex.map(code ->
-                ShaderCodeTransformation.transform(
-                    Program.Type.VERTEX,
-                    "iris_sodium_terrain_vertex",
-                    code
-                )
-            );
-            translucentVertex = translucentVertex.map(code ->
-                ShaderCodeTransformation.transform(
-                    Program.Type.VERTEX,
-                    "iris_sodium_terrain_vertex",
-                    code
-                )
-            );
-        }
-        else {
-            Helper.err("iris terrain shader ImmPtl patched twice");
-        }
-    }
+//    @Shadow
+//    Optional<String> terrainSolidVertex;
+//
+//    @Shadow
+//    Optional<String> terrainCutoutVertex;
+//
+//    @Shadow
+//    Optional<String> translucentVertex;
+//
+//    @Unique
+//    private boolean immptlPatched = false;
+//
+//    @Inject(
+//        method = "patchShaders",
+//        at = @At("RETURN")
+//    )
+//    private void onPatchShaderEnds(ChunkVertexType chunkVertexType, CallbackInfo ci) {
+//        if (!immptlPatched) {
+//            immptlPatched = true;
+//            terrainSolidVertex = terrainSolidVertex.map(code ->
+//                ShaderCodeTransformation.transform(
+//                    Program.Type.VERTEX,
+//                    "iris_sodium_terrain_vertex",
+//                    code
+//                )
+//            );
+//            terrainCutoutVertex = terrainCutoutVertex.map(code ->
+//                ShaderCodeTransformation.transform(
+//                    Program.Type.VERTEX,
+//                    "iris_sodium_terrain_vertex",
+//                    code
+//                )
+//            );
+//            translucentVertex = translucentVertex.map(code ->
+//                ShaderCodeTransformation.transform(
+//                    Program.Type.VERTEX,
+//                    "iris_sodium_terrain_vertex",
+//                    code
+//                )
+//            );
+//        }
+//        else {
+//            Helper.err("iris terrain shader ImmPtl patched twice");
+//        }
+//    }
 }
